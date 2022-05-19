983d19cd91a1649b2537aa964e86b43b6f01e58f
21shunya <shabanchick@gmail.com>
2022-05-19 18:08:09 +0300

    feat(app): animation on background

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import Activity from './Activity/Activity';
 function App() {
 
   return (
-    <div className="body-wrap">
-      <StaticInfo />
-      <Activity />
+    <div className='a'>
+      <img className="el-1-1" src="el_1_1.svg" />
+      <img className="el-1-2" src="el_1_2.svg" />
+      <img className="el-2-1" src="el_2_1.svg" />
+      <img className="el-2-2" src="el_2_2.svg" />
+      <img className="el-3-1" src="el_3_1.svg" />
+      <img className="el-3-2" src="el_3_2.svg" />
+      <div className='a'></div>
+      <div className="body-wrap">
+        <StaticInfo />
+        <Activity />
+      </div>
     </div>
   );
 }
---
+++ src/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+
+`;
+
+export default function d() {
+  return <StyledDiv></StyledDiv>;
+}
